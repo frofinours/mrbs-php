69ffaf2201add2ae269f75faf29a70a06e0f159c
Zakarya TOLBA <zakarya.tolba@icof.fr>
2018-11-20 15:45:11 +0100

    Ajout du fichier PDO.php
    supression d'un fichier index.php au passage

---
+++ PDO.php
@@ -0,0 +1,20 @@
+
+<?php
+$PARAM_hote='localhost';
+$PARAM_port='8081';
+$PARAM_nom_bd='mrbs';
+$PARAM_utilisateur='root';
+$PARAM_mot_passe='mdp';
+
+try
+{
+        $connexion = new PDO('mysql:host='.$PARAM_hote.';dbname='.$PARAM_nom_bd, $PARAM_utilisateur, $PARAM_mot_passe);
+		$connexion->exec('SET NAMES utf8');
+}
+
+catch(Exception $e)
+{
+        echo 'Une erreur de connexion à la BDD est survenue !';
+        die();
+}
+?>
