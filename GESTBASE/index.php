73a3ac5ed0369dc890d781fdf01d56274e1097b3
Louise <45199211+OrnithorynqueVioletUnijambiste@users.noreply.github.com>
2019-06-04 14:31:09 +0200

    Gros push sa grand mère
    Superkebab44

---
+++ GESTBASE/index.php
@@ -0,0 +1,75 @@
+<?php 
+require('controller/controller.php');
+
+//On vérifie si une action existe
+if (isset($_GET['action'])) 
+{
+    if ($_GET['action'] == 'utilisateurs') {
+        getUtilisateurs();
+    }
+    if ($_GET['action'] == 'ConnexionU') {
+        getConnexion($_POST['username'], $_POST['password']);
+    }
+    if ($_GET['action'] == 'DeconnexionU') {
+        getDeconnexion();
+    }
+    if ($_GET['action'] == 'menuAssoc') {
+        menuAssoc();
+    }
+
+    /** Droits admin nécessaire**/
+
+    if ($_GET['action'] == 'AjouterU') {
+        if(isset($_COOKIE['Level']) && $_COOKIE['Level'] == 1){
+            addUtilisateur();
+        }
+        else
+        {
+            require('view/template/403.php');
+        }
+    }
+    if ($_GET['action'] == 'ModifierU') {
+        if(isset($_COOKIE['Level']) && $_COOKIE['Level'] == 1){
+            editUtilisateurId($_GET['id']);
+        }
+        else
+        {
+            require('view/template/403.php');
+        }
+    }
+
+    if ($_GET['action'] == 'creerAssoc') {
+        if(isset($_COOKIE['Level']) && $_COOKIE['Level'] == 1){
+            creationAssoc();
+        }
+        else
+        {
+            require('view/template/403.php');
+        }
+    }
+    if ($_GET['action'] == 'modifAssoc') {
+        if(isset($_COOKIE['Level']) && $_COOKIE['Level'] == 1){
+            modifAssoc(/*$_GET['id']*/);
+        }
+        else
+        {
+            require('view/template/403.php');
+        }
+    }
+    
+}
+else
+{
+    //On vérifie l'existence d'un cookie
+    if (isset($_COOKIE['Utilisateur']) && $_COOKIE['Utilisateur'] != null)
+    {
+        //Page d'accueil
+        require('view/Login/ViewAfterLogin.php');
+    }
+    else
+    {
+        //Portail de connexion
+        require('view/Login/ViewLogin.php');
+    }
+}
+?>
---
+++ GESTBASE/model/getEmail.php
@@ -0,0 +1,28 @@
+<?php
+    include 'PDO.php';
+    $id_res = $_POST['id_res'];
+    $bdd = connexionBDD();
+    if(isset($id_res)){
+        $requete = "SELECT email FROM mrbs_users WHERE id = '".$id_res."'";
+       
+        $email = null;
+        try {
+            $resultat = $bdd->query($requete);
+            $resultat->setFetchMode(PDO::FETCH_OBJ);
+           
+            while ($ligne = $resultat->fetch()) 
+            {
+                $email = $ligne;
+            }
+            $resultat->closeCursor();
+        } 
+        catch (Exception $e) {
+            die("Erreur : " .$e->getMessage());
+        }
+        
+        echo $email->email;
+    
+    }
+        
+ 
+?>
