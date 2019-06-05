8cb2a07bf0c731f8fdf828ba2eded9d16260a8c6
Louise <45199211+OrnithorynqueVioletUnijambiste@users.noreply.github.com>
2019-06-05 16:36:16 +0200

    Modification (très vite fait) du CSS pour 403.php
    Le CSS c'est un peu de la merde quand même.hyju_

--- GESTBASE/view/template/403.php
+++ GESTBASE/view/template/403.php
@@ -3,11 +3,14 @@
     <head>
         <meta charset="UTF-8">
         <title>403</title>
+        <link rel="stylesheet" type="text/css" media="screen" href="css/style.css" />
     </head>
-    <body>
-        <h1>Accès interdit</h1>
-        <p>Vous n'êtes pas autorisé à consulter cette page.</p>
-        <a id="Return" href="view/template/JavascriptError.php" onclick="HomeReturn();return false;">Retourner à la page d'accueil</a>
+    <body class="accessDenied">
+        <div class="Denied">
+            <h1>Accès interdit</h1>
+            <p>Vous n'êtes pas autorisé à consulter cette page.</p>
+            <a id="Return" href="view/template/JavascriptError.php" onclick="HomeReturn();return false;">Retourner à la page d'accueil</a>
+        </div>
     </body>
     <script src="js/Template/Redirection.js"></script>
 </html>
