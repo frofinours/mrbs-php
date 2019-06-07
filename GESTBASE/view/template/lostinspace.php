5af605e0f412af26f18c6a26c0fdbd87b8643c53
Zakarya TOLBA <zakarya.tolba@icof.fr>
2019-06-07 17:06:17 +0200

    Update lostinspace.php

--- GESTBASE/view/template/lostinspace.php
+++ GESTBASE/view/template/lostinspace.php
@@ -1,8 +1,11 @@
-<link rel="stylesheet" href="../css/style.css" media="screen" />
+<link rel="stylesheet" href="css/style.css" media="screen" />
 
 <body class="formulaire"> 
 
   <div class="form">
-  <h1>Gestion des salles</h1>
-  <p>Erreur 404, cette page n'existe pas!</p>
+  <h1>Erreur 404</h1>
+  <p>Cette page n'existe pas!</p>
   <input type='button' id='buttonRetour' value="<- Retour" onclick="document.location.href='index.php'"/><br>
+
+</div>
+</body>
